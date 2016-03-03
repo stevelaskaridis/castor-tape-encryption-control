39d190cb2be8d5fa8186b88bda2f0a06d3c61a3d
Stefanos Laskaridis <steve.laskaridis@cern.ch>
2016-03-03 14:34:44 +0100

    Corrections and documentation on python script

--- src/castor-tape-encryption-control.py
+++ src/castor-tape-encryption-control.py
@@ -1,12 +1,41 @@
 #! /usr/bin/env python
 
+# ******************************************************************************
+#
+#  This an external script to be used with and from CASTOR project.
+#  See http://castor.web.cern.ch/castor
+#
+#  Copyright (C) 2016  CERN
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Affero General Public License
+#  as published by the Free Software Foundation.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#
+#
+#  @author Stefanos Laskaridis, steve.laskaridis@cern.ch
+# ****************************************************************************/
+
 import argparse
 import json
 import os
 
 
-def extract_pool_name():
-    command = ['vmgrlisttape', '-V', args.vid]
+def extract_pool_name(vid):
+    """
+    Extract the pool name based the vid of the tape.
+    It's results are based on the output of `vmgrlisttape` tool.
+    :param vid: The VID of the tape whose pool we are searching for.
+    :return: The pool name of the given VID or the return code of the `vmgrlisttape`
+    command if != 0.
+    """
+    command = ['vmgrlisttape', '-V', vid]
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     pool_cmd_out, pool_cmd_err = proc.communicate()
     if proc.returncode == 0:
@@ -14,7 +43,13 @@ def extract_pool_name():
     else:
         return proc.returncode
 
+
 def cleanup_and_exit(drive):
+    """
+    Function clearing the encryption parameters from the drive. Exits the program with code -1
+    after completion.
+    :param drive: The drive whose encryption parameters are to be cleared.
+    """
     print "Reverting drive to non-encrypting state."
     if disable_encryption(drive) == 0:
         exit(-1)
@@ -24,6 +59,12 @@ def cleanup_and_exit(drive):
 
 
 def disable_encryption(drive):
+    """
+    Disable the encryption and clear all encryption parameters from the drive.
+    Calls C++ backend in order to communicate with the drive via the SCSI interface.
+    :param drive: The drive whose encryption parameters are to be cleared.
+    :return: The backend's process return code.
+    """
     if drive:
         command = ['../bin/spout_cmd', '-d', drive, '-n']
         proc = subprocess.Popen(command, close_fds=True)
@@ -32,17 +73,27 @@ def disable_encryption(drive):
 
 
 def enable_encryption(key, drive):
+    """
+    Enable the encryption and pass encryption parameters to the drive.
+    Calls C++ backend in order to communicate with the drive via the SCSI interface.
+    :param key: The key with which the drive will be encrypting/decrypting data.
+    :param drive: The drive whose encryption parameters are to be cleared.
+    :return: The backend's process return code.
+    """
     if key:
         command = ['../bin/spout_cmd', '-d', drive, '-k', key]
         proc = subprocess.Popen(command, close_fds=True)
         proc.communicate()
 
 
+# Entry point of execution.
 if __name__ == '__main__':
+
+    # Parse CLI arguments. Adheres to the specification under /docs/interface_specification.md
     parser = argparse.ArgumentParser()
     action_group = parser.add_mutually_exclusive_group(required=True)
     action_group.add_argument('--enable', dest='enable', action='store_true',
-                          help="Enable the encryption for the target device.")
+                              help="Enable the encryption for the target device.")
     action_group.add_argument('--disable', dest='disable', action='store_true',
                               help="Clear any encryption parameters from the target device.")
     parser.add_argument('-D', '--device', dest='drive', required=True,
@@ -65,14 +116,13 @@ if __name__ == '__main__':
         print "Turning encryption on in drive."
 
         # Get pool of the tape
-        pool_name = extract_pool_name()
+        pool_name = extract_pool_name(args.vid)
         if type(pool_name) == int:
-            print "Could not determine the pool to which this VID belongs."
+            print "Could not determine the pool to which this VID belongs."gi
             exit(-1)
 
         # Get encryption key from file
         key = ""
-        #/etc/castor/encryption/poolName_vidName
         key_filename = '/etc/castor/encryption/keys/tape-encryption-keys.json'
         if os.path.exists(key_filename):
             with open(key_filename) as f:
@@ -82,7 +132,8 @@ if __name__ == '__main__':
                     print ' '.join([key_filename, 'is not a valid JSON file.'])
                     exit(-1)
                 except KeyError:
-                    print ' '.join(['Pool name',pool_name, 'is not available in the JSON key store.'])
+                    print ' '.join(['Pool name', pool_name,
+                                    'is not available in the JSON key store.'])
                     exit(-1)
                 except:
                     print ' '.join(['Unknown error while reading the file', key_filename])
@@ -92,7 +143,8 @@ if __name__ == '__main__':
             exit(-1)
 
         if enable_encryption(key, args.drive) == 0:
-            print 'Encryption enabled. Using key from file {filename}.'.format(filename=key_filename)
+            print 'Encryption enabled. ' \
+                  'Using key from file {filename}.'.format(filename=key_filename)
         else:
             print 'Enabling encryption failed.'
             cleanup_and_exit(args.drive)
@@ -102,4 +154,4 @@ if __name__ == '__main__':
         if disable_encryption(args.drive) == 0:
             print 'Encryption disabled. Cleared encryption parameters from tape.'
         else:
-            print 'Disabling encrption failed.'
+            print 'Disabling encryption failed.'
